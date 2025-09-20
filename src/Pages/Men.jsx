f09d768223c64dd74a9a1b7b2216b006dffecaf7
ibtasam-jamali <ibtasamjamali@gmail.com>
2025-09-20 14:51:59 +0500

    new  update

--- src/Pages/Men.jsx
+++ src/Pages/Men.jsx
@@ -2,23 +2,22 @@ import React, { useState } from "react";
 import styles from "../styles/Mens.module.css";
 import Card from "../Components/Card";
 import { menProducts } from "../Data/Products";
+import SortIcon from "@mui/icons-material/Sort"; // Import the sort icon
 
 const Men = () => {
+  const [sortOption, setSortOption] = useState("");
 
+  const handleSortChange = (e) => {
+    setSortOption(e.target.value);
+  };
 
-    const [sortOption, setSortOption] = useState("");
-
-    const handleSortChange =(e) => {
-      setSortOption(e.target.value);
-    }
-
-    const sortedProducts = [...menProducts].sort((a, b) => {
-      if(sortOption === "lowToHigh") return a.price - b.price
-      if(sortOption === "highToLow") return b.price - a.price
-      if(sortOption === "az") return a.title.localeCompare(b.title)
-      if(sortOption === "za") return b.title.localeCompare(a.title)
-      return 0;
-    })
+  const sortedProducts = [...menProducts].sort((a, b) => {
+    if (sortOption === "lowToHigh") return a.price - b.price;
+    if (sortOption === "highToLow") return b.price - a.price;
+    if (sortOption === "az") return a.title.localeCompare(b.title);
+    if (sortOption === "za") return b.title.localeCompare(a.title);
+    return 0;
+  });
 
   return (
     <section className={styles.menSection}>
@@ -28,16 +27,20 @@ const Men = () => {
       </div>
 
       <div className={styles.sortBox}>
-        <label>Sort by:</label>
-        <select value={sortOption} onChange={handleSortChange}>
-        <option value="">Default</option>
-        <option value="lowToHigh">Price: Low to High</option>
-        <option value="highToLow">Price: High to Low</option>
-        <option value="az">Name: A to Z</option>
-        <option value="za">Name: Z to A</option>
+        <SortIcon className={styles.sortIcon} />
+        {/* <span className={styles.sortText}>Sort</span> */}
+        <select
+          value={sortOption}
+          onChange={handleSortChange}
+          className={styles.sortSelect}
+        >
+          <option value="">Default</option>
+          <option value="lowToHigh">Price: Low to High</option>
+          <option value="highToLow">Price: High to Low</option>
+          <option value="az">Name: A to Z</option>
+          <option value="za">Name: Z to A</option>
         </select>
       </div>
-
 
       <Card products={sortedProducts} />
     </section>
